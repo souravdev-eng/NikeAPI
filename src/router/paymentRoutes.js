c75f5c4c74c273e5804f0980555eaaed415b6c70
sourav_allmysons <souravmajumdar.dev@gmail.com>
2023-06-28 11:44:22 +0530

    Complete the stripe payment

--- src/router/paymentRoutes.js
+++ src/router/paymentRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const stripe = require('stripe')(process.env.STRIPE_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 // router endpoints
 router.post('/intents', async (req, res) => {
@@ -8,13 +8,13 @@ router.post('/intents', async (req, res) => {
     // create a PaymentIntent
     const paymentIntent = await stripe.paymentIntents.create({
       amount: req.body.amount, // Integer, usd -> pennies, eur -> cents
-      currency: 'usd',
+      currency: 'inr',
       automatic_payment_methods: {
         enabled: true,
       },
     });
     // Return the secret
-    res.json({ paymentIntent: paymentIntent.client_secret });
+    res.json({ paymentIntent: paymentIntent?.client_secret });
   } catch (e) {
     res.status(400).json({
       error: e.message,
